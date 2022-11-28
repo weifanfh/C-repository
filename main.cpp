dd544ff37620238a9075aec68064502283017640
fanghui <3142361906@qq.com>
2022-11-28 23:45:53 +0800

    成功进行编译设置以将头文件分装到不同文件夹中

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-#include "symbol_example.h"
-#include "system_exchange_example.h"
+#include "system_example/symbol_example.h"
+#include "system_exchange_example/system_exchange_example.h"
 
 int main()
 
 {
+    // system("chcp 65001");
     show_system();
-
     return 0;
 
 }
